c413cb8292d23631f0f9f7743cb06c94abba4fa3
Gabriel S <desouza.gabriel@icloud.com>
2021-01-10 18:03:53 -0300

    feat: asdf script

---
+++ bin/tools/asdf.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+sudo apt-get install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
+
+case $SHELL in
+  */zsh)
+      # assume Zsh
+      echo 'autoload bashcompinit' >> ~/.zshrc
+      echo 'bashcompinit' >> ~/.zshrc
+      echo 'source $HOME/.asdf/asdf.sh' >> ~/.zshrc
+      echo 'source $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+    ;;
+  */bash)
+      echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+      echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+    ;;
+esac
+
+# Install useful plugins
+echo "[INFO] Installing asdf plugins...";
+source $HOME/.asdf/asdf.sh;
+
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
+asdf plugin-add node https://github.com/asdf-vm/asdf-nodejs.git;
