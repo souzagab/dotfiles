839efb6139814c85793e847f19ce9a00c882d4d0
Gabriel S <thesouzagab@gmail.com>
2021-12-25 10:49:45 -0300

    chore: changing to use ansible-pull

--- bin/copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in $CONFIG_PATH/.[^.]*; do
-# for file in ../config/.[^.]*; do 
-  echo "Copying ${file}"
-  cp -f $file ~/ 
-done
--- bin/programs/bitwarden.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/bash
-
-# TODO: Install from source, avoid snapd
-
-sudo apt update
-# sudo apt install snapd
-sudo snap install bitwarden
